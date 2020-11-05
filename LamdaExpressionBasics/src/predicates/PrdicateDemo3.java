687ffbe23b762ea441bca08ab01abb82a57b9b80
Damith Samarakoon <damithnsamarakoon@gmail.com>
2020-11-06 00:02:10 +0530

    predicate joined

---
+++ LamdaExpressionBasics/src/predicates/PrdicateDemo3.java
@@ -0,0 +1,21 @@
+package predicates;
+
+import java.util.function.Predicate;
+
+public class PrdicateDemo3 {
+    public static void main(String[] args) {
+        int a[] = {5,10,15,20,25,30,35,40,45,50};
+
+        Predicate<Integer> p1 = i -> i%2==0;
+        Predicate<Integer> p2 = i -> i>10;
+
+        for (int n:a){
+//            if (p1.test(n) && p2.test(n)){
+//                System.out.println(n);
+//            }
+            if (p1.and(p2).test(n)){
+                System.out.println(n);
+            }
+        }
+    }
+}
